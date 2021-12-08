389825c4aa6444bc766dccfdd9763acb7c30cf28
Adil <adilsattar674@gmail.com>
2021-12-08 15:20:26 +0500

    Initial commit

---
+++ app/src/main/java/com/aadi/fansy/yummylib/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aadi.fansy.yummylib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ yummy/src/main/java/com/aadi/fansy/yummy/Yummy.java
@@ -0,0 +1,10 @@
+package com.aadi.fansy.yummy;
+
+import android.util.Log;
+
+public class Yummy {
+
+    public static void showCake(String name){
+        Log.d("ADIL"  , "I am display name of cake: " + name);
+    }
+}
